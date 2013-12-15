97f2601fc54b96e0c9cf68d677e4bf5295172403
ziggi <xziggix@gmail.com>
2013-12-15 23:55:09 +0400

    Fixes #4: add access for remote uploads

--- upload.php
+++ upload.php
@@ -4,6 +4,10 @@ include 'upload.class.php';
 
 $files_array = $_FILES['files'];
 
+// access for remote uploads
+header('Access-Control-Allow-Origin: *');
+
+// upload files
 $upload = new Upload($files_array);
 $upload->upload();
 
