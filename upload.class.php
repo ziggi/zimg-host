945e7e59df771969bb67f416788f45603e3b14c0
Anton <admin@stv-fian.ru>
2014-03-31 16:22:16 +0400

    Update upload.class.php
    Добавил разрешение

--- upload.class.php
+++ upload.class.php
@@ -31,6 +31,7 @@ class Upload {
 
 			$img_size = getimagesize($temp_name);
 			$array_result[$i]['type'] = $img_size['mime'];
+			$array_result[$i]['size'] = $img_size;
 
 			if (!$this->is_support_type($array_result[$i]['type'])) {
 				$array_result[$i]['error']['upload'] = 1;
@@ -94,6 +95,8 @@ class Upload {
 			move_uploaded_file($files[$i]['tmp_name'], __DIR__ . '/file/' . $new_name);
 
 			$array_result[$i]['url'] = $new_name;
+			$img_size = getimagesize( __DIR__ . '/file/' . $new_name);
+			$array_result[$i]['size'] = $img_size;
 		}
 		echo json_encode($array_result);
 	}
