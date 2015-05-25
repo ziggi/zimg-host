3da3b35b448f045d948542d960738bb3da376c38
ziggi <xziggix@gmail.com>
2015-05-25 16:59:49 +0300

    added url validation

--- bootstrap.php
+++ bootstrap.php
@@ -14,7 +14,9 @@ if ($count == 1) {
 	$img_file = 'file/' . $img_file[0] . '/' . $img_file[1] . '/' . $img_file;
 }
 
-if (file_exists($img_file)) {
+$is_valid_file = preg_match('/^file\/[0-9a-f]\/[0-9a-f]\/[0-9a-f]{32}\.[a-z]+$/', $img_file) == 1;
+
+if ($is_valid_file && file_exists($img_file)) {
 	$img_info = getimagesize($img_file);
 
 	header('Content-type: ' . $img_info['mime']);
