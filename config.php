600fe3506c028bf026f093f449bc5248226677a5
ziggi <xziggix@gmail.com>
2015-06-11 13:20:20 +0300

    fixed detection of the current directory

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return array(
 	// template folder name under /tpl
 	'tpl' => 'default',
 	// uri addres to site
-	'uri' => '//' . $_SERVER['HTTP_HOST'] . str_replace($_SERVER['DOCUMENT_ROOT'], '', dirname(__FILE__)),
+	'uri' => '//' . preg_replace('#/$#', '', $_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF'])),
 	// params
-	'uri_param' => preg_replace('#^' . dirname($_SERVER['PHP_SELF']) . '[/]?#', '', $_SERVER['REQUEST_URI'], 1),
+	'uri_param' => preg_replace('#^' . dirname($_SERVER['PHP_SELF']) . '/?#', '', $_SERVER['REQUEST_URI'], 1),
 );
