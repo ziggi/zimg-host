07fe535f3b2c6a9441f9986a6179abc782264a1a
Sergey Marochkin <xziggix@gmail.com>
2015-10-08 00:53:54 +0300

    Version information moved to config.php

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?php
 
 return array(
+	// version
+	'version' => '2.1.4',
 	// template folder name under /tpl
-	'tpl' => 'default',
+	'tpl' => 'material',
 	// uri addres to site
 	'uri' => '//' . preg_replace('#/$#', '', $_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF'])),
 	// params
