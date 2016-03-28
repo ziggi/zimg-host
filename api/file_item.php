7058234419d37050a4e06a0f0fd8bf55297796d0
Sergei Marochkin <me@ziggi.org>
2016-03-28 19:54:55 +0300

    Set UTF-8 as default encoding for htmlentities function

--- api/file_item.php
+++ api/file_item.php
@@ -2,11 +2,11 @@
 
 $conf = include '../config.php';
 
-$url = htmlentities($_POST['url']);
-$thumburl = htmlentities($_POST['thumbUrl']);
-$name = htmlentities($_POST['name']);
-$width = htmlentities($_POST['size']['width']);
-$height = htmlentities($_POST['size']['height']);
+$url = htmlentities($_POST['url'], null, 'UTF-8');
+$thumburl = htmlentities($_POST['thumbUrl'], null, 'UTF-8');
+$name = htmlentities($_POST['name'], null, 'UTF-8');
+$width = htmlentities($_POST['size']['width'], null, 'UTF-8');
+$height = htmlentities($_POST['size']['height'], null, 'UTF-8');
 $filesize = round($_POST['size']['filesize'] / 1024, 2);
 
 include sprintf('../tpl/%s/%s.php', $conf['tpl'], pathinfo(__FILE__, PATHINFO_FILENAME));
--- api/file_item_error.php
+++ api/file_item_error.php
@@ -2,7 +2,7 @@
 
 $conf = include '../config.php';
 
-$name = htmlentities($_POST['name']);
-$error = htmlentities($_POST['error']);
+$name = htmlentities($_POST['name'], null, 'UTF-8');
+$error = htmlentities($_POST['error'], null, 'UTF-8');
 
 include sprintf('../tpl/%s/%s.php', $conf['tpl'], pathinfo(__FILE__, PATHINFO_FILENAME));
